039b1bce2243576d8f8312522766b0fcf729b190
a-reny <123300590+a-reny@users.noreply.github.com>
2025-03-02 21:40:57 -0600

    Name changes in schema

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,5 +1,5 @@
 import {db} from "~/server/db"
-import {files as filesSchema, folders as foldersSchema} from "~/server/db/schema"
+import {files_table as filesSchema, folders_table as foldersSchema} from "~/server/db/schema"
 import DriveContents from "../../drive-contents";
 import { eq } from "drizzle-orm";
 
@@ -26,7 +26,7 @@ export default async function GoogleDriveClone(props: {params: Promise<{folderId
     const filesPromise = db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
     const foldersPromise = db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
 
-    const parentsPromise = getAllParents(parsedFolderId)
+    const parentsPromise = getAllParents(parsedFolderId);
 
     const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise]);
     return <DriveContents files={files} folders={folders} parents={parents} />
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,7 +4,7 @@ import { int, text, index, singlestoreTableCreator, bigint } from
 
 export const createTable = singlestoreTableCreator((name) => `drive_${name}`,);
 
-export const files = createTable("files_table", {
+export const files_table = createTable("files_table", {
   id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
   name: text("name").notNull(),
   size: int("size").notNull(),
@@ -15,7 +15,7 @@ export const files = createTable("files_table", {
 },
 )
 
-export const folders = createTable("folders_table", {
+export const folders_table = createTable("folders_table", {
   id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
   name: text("name").notNull(),
   parent: bigint("parent", { mode: "number", unsigned: true }),
