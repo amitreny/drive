2f6d6a4eb7baf3a167712244c9e56ae35318e6ac
a-reny <123300590+a-reny@users.noreply.github.com>
2025-03-04 19:42:46 -0600

    Upload testing

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,28 +1,59 @@
 import "server-only";
 
-import {db} from "~/server/db"
-import {files_table as filesSchema, folders_table as foldersSchema} from "~/server/db/schema"
+import { db } from "~/server/db";
+import {
+  files_table as filesSchema,
+  folders_table as foldersSchema,
+  DB_FileType,
+  DB_FolderType,
+} from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 export const QUERIES = {
-    getFolders: function (folderId: number){
-        return db.select().from(foldersSchema).where(eq(foldersSchema.parent, folderId));
-        },
+  getFolders: function (folderId: number) {
+    return db
+      .select()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.parent, folderId));
+  },
 
-    getFiles: function (folderId: number){
-        return db.select().from(filesSchema).where(eq(filesSchema.parent, folderId));
-        },
-        
-    getAllParentsForFolder:async function (folderId: number) {
+  getFiles: function (folderId: number) {
+    return db
+      .select()
+      .from(filesSchema)
+      .where(eq(filesSchema.parent, folderId));
+  },
+
+  getAllParentsForFolder: async function (folderId: number) {
     const parents = [];
     let currentId: number | null = folderId;
     while (currentId !== null) {
-        const folder = await db.selectDistinct().from(foldersSchema).where(eq(foldersSchema.id, currentId));
-        if (!folder[0]){
-            throw new Error("Parent folder not found");
-        }
-        parents.unshift(folder[0]);
-        currentId = folder[0]?.parent;
+      const folder = await db
+        .selectDistinct()
+        .from(foldersSchema)
+        .where(eq(foldersSchema.id, currentId));
+      if (!folder[0]) {
+        throw new Error("Parent folder not found");
+      }
+      parents.unshift(folder[0]);
+      currentId = folder[0]?.parent;
     }
     return parents;
-}, 
+  },
+};
+
+export const MUTATIONS = {
+  createFile: async function (input: {
+    file: {
+      name: string;
+      size: number;
+      url: string;
+      parent: 0;
+    };
+    userId: string;
+  }) {
+    return await db.insert(filesSchema).values({
+        ...input.file,
+        parent:input.file.parent,
+    });
+  },
 };
